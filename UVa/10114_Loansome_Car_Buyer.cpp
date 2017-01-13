1fe268f93d75794ac06c311eb8ebebd50e2cccda
bishal1995 <bishalpaul1905@gmail.com>
2017-01-13 19:19:49 +0530

    Loansome problem basic structure declared

---
+++ UVa/10114_Loansome_Car_Buyer.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<utility>
+#include<vector>
+using namespace std;
+// Processing each test case
+void check_month( int ld ){
+  int loan_duration = ld,depreciation_record_count = 0;
+  float down_payment = 0.0,loan_amount = 0.0;
+  vector<pair<int,float>> depreciation_record; 
+  cin>>down_payment>>loan_amount>>depreciation_record_count;
+  for( int i = 0 ; i < depreciation_record_count ; ++i ){
+    pair<int,float> buff;
+    cin>>buff.first>>buff.second;
+    depreciation_record.push_back( buff );
+
+
+    
+  }
+
+}
+
+int main(){
+  int lld=0;
+  cin >> lld;
+  while( lld >= 0 ){
+    check_month( lld );
+    cin>>lld;
+  }
+  return 0;
+}
+
+
+
+/*
+30 500.0 15000.0 3
+0 .10
+1 .03
+3 .002
+12 500.0 9999.99 2
+0 .05
+2 .1
+60 2400.0 30000.0 3
+0 .2
+1 .05
+12 .025
+-99 0 17000 1
+*/
