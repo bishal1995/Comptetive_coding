302f377c83d74c82267f7f89a05fdba0f6eb5721
bishal1995 <bishalpaul1905@gmail.com>
2017-01-26 19:45:56 +0530

    Unique sub array solution designed - buggy

--- arrays/codelog031116unique_subarrays.cpp
+++ arrays/codelog031116unique_subarrays.cpp
@@ -1,48 +1,46 @@
 #include<iostream>
-#include<vector>
+#include<set>
+#include<string>
 using namespace std;
+
+// Returning unique array size
+int unique_array_weight( int size ){
+  return (size*(size+1)*(size+2))/6;
+}
+// Getting weight of array
+int weight( string input ){
+  int size = input.length(),i=1,start=0,end=0 ;
+  int weight = 0;
+  set<char> test;
+  auto status = test.insert(input[0]);
+  while( i<size ){
+    status = test.insert(input[i]);
+    if( status.second ){
+      // found repeated element
+      end = i;
+      int diff = end - start;
+      cout<<diff<<"\n";
+      weight += unique_array_weight(diff);
+      test.clear();
+      start = i;      
+    }
+    else{
+      continue;
+    }
+    ++i;
+  }
+  return weight;  
+}
+
+
 int main(){
-
-  vector<int> array;
-  int N,buff,MAX=0;
-  cin>>N;
-  for(int i=0;i<N;++i){
-    cin>>buff;
-    array.push_back(buff);
-    if(buff>MAX)
-      MAX=buff;
-  }
-  vector<vector<int>> indexes (MAX,vector<int>(0)) ;
-  vector<vector<int>> repeat_index;
-  vector<vector<int>> pair;
-  vector<int> test;
+  string str = "abcde";
+  int value = weight(str);
+  cout<<value<<"\n";
+   
 
   
 
   
-  for(int j=0;j<N;++j){
-    indexes[array[j]].push_back(j);
-  } 
-
-  /*
-  for(int k=0;k<MAX;++k){
-    if( indexes[k].size() > 1 )
-      repeat_index.push_back(indexes[k]);
-  }
-  int repeat_size = repeat_index.size();
-  for(int l=0;l<repeat_size;++l){
-    int size = repeat_index[l].size();
-    for(int m=0;m<size;++m)
-      for(int n=m+1;n<size;++n){
-	pair.push_back({m,n});
-      }
-  }
-  for(int o=0;o<pair.size();++o){
-    cout<<pair[o][0]<<" "<<pair[0][1]<<endl;
-  } 
-  */
-  
-
-
   return 0;
 }
