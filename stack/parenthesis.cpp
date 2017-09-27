9b4ef3f323c806c381df9d762818a604835572c3
Bishal Paul <bishalpaul1905@gmail.com>
2017-09-27 19:58:48 +0530

    File edited for upload

--- stack/parenthesis.cpp
+++ stack/parenthesis.cpp
@@ -7,24 +7,25 @@ int main(){
   cin>>size;
   for( int i = 0 ; i < size ; ++i ){
     if( sequence.size() > 0 ){
-      cout<<"Non-empty sequence\n";
+      //cout<<"Non-empty sequence\n";
       cin>>buffer;
       if( (buffer*-1) == sequence.top() ){
-	cout<<"Balancing element\n";
+	//cout<<"Balancing element\n";
 	sequence.pop();
       }
       else{
-	cout<<"Non-balancing element\n";
+	//cout<<"Non-balancing element\n";
 	sequence.push(buffer);
       }
     }
     else{
-      cout<<"Empty sequrnce\n";
+      //cout<<"Empty sequrnce\n";
       cin>>buffer;
       sequence.push(buffer);
     }
   }
   balanced_sequence_size = size - sequence.size();
-  cout<<"The size of balanced sequence is : "<<balanced_sequence_size<<"\n";
+  //cout<<"The size of balanced sequence is : "<<balanced_sequence_size<<"\n";
+  cout<<balanced_sequence_size;
   return 0;
 }
