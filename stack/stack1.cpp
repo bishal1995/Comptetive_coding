0fd814d424e26bcf2fde158a1874ef9007311025
Bishal Paul <bishalpaul1905@gmail.com>
2017-09-27 14:00:15 +0530

    STack basic implementaion using C++ STL library's special containers

---
+++ stack/stack1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+  stack<int> s1;
+  int buffer;
+  for ( int i=0 ; i < 5 ; i++ ){
+    cin>>buffer;
+    s1.push(buffer);
+  }
+  cout<<"\nSize of stack after taking input is : "<<s1.size()<<"\n";
+  while( !s1.empty() ){
+    cout<<s1.top()<<" - ";
+    s1.pop();
+  }
+  cout<<"\nSize of stack after emptying is : "<<s1.size()<<"\n";
+  return 0;
+
+}
