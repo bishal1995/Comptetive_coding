c5e9a5c57100b054eba85324b0c95570c0d0e21e
Bishal Paul <bishalpaul1905@gmail.com>
2017-09-27 19:06:02 +0530

    Parenthesis balancing done

---
+++ stack/parenthesis.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+  int size,buffer,balanced_sequence_size;
+  stack<int> sequence;
+  cin>>size;
+  for( int i = 0 ; i < size ; ++i ){
+    if( sequence.size() > 0 ){
+      cout<<"Non-empty sequence\n";
+      cin>>buffer;
+      if( (buffer*-1) == sequence.top() ){
+	cout<<"Balancing element\n";
+	sequence.pop();
+      }
+      else{
+	cout<<"Non-balancing element\n";
+	sequence.push(buffer);
+      }
+    }
+    else{
+      cout<<"Empty sequrnce\n";
+      cin>>buffer;
+      sequence.push(buffer);
+    }
+  }
+  balanced_sequence_size = size - sequence.size();
+  cout<<"The size of balanced sequence is : "<<balanced_sequence_size<<"\n";
+  return 0;
+}
